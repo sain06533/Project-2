0e4cfe1bbb7d16a88b8f50488fc2767bcef4c4f3
sain06533 <sain06533@gmail.com>
2023-03-27 11:21:38 +0530

    changed

---
+++ ent-apk/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function home() {
+  return (
+    <header>
+      <div className='head'>Home</div>
+      <div className='head1'>Products</div>
+      <div className='head1'>Contact Us!</div>
+    </header>
+  )
+}
+
+export default home
